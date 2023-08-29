f105c0fdf4fe194a9cafb3cc16bf60a65bd9741b
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-08-28 18:06:08 -0700

    Finish grid logic and appended info to DOM in AppFunctional.js

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -8,15 +8,21 @@ const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
-  const [selectedIndex, setSelectedIndex] = useState(4)
+  const [selectedIndex, setSelectedIndex] = useState(initialIndex)
+  const [stepCount, setStepCount] = useState(initialSteps)
+  const [message, setMessage] = useState(initialMessage)
   const [email, setEmail] = useState(initialEmail)
 
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+  function getXY(reset) {
+    if (reset === 'reset') {
+      setSelectedIndex(initialIndex)
+      setMessage('')
+      setStepCount(0)
+    }
+
     let xMap = {'0': 1, '1': 2, '2': 3, '3': 1, '4': 2, '5': 3, '6': 1, '7': 2, '8': 3}
     let coordinates = {x: 0, y: 0}
     if (selectedIndex >= 0 && selectedIndex <= 2) {
@@ -29,28 +35,62 @@ export default function AppFunctional(props) {
       const y = 3
       coordinates = {x: xMap[selectedIndex], y: y}
     } 
-   
+    
+    return coordinates
   }
 
   function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+    const coordinates = getXY()
+    return `Coordinates (${coordinates.x}, ${coordinates.y})`
   }
 
-  function reset() {
+  function reset(reset) {
     // Use this helper to reset all states to their initial values.
+    getXY(reset)
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    let coordinates = getXY()
+
+    let checkMove = `${coordinates.x}, ${coordinates.y} : ${direction}`
+    const nonMovableMap = {
+      '1, 1 : left' : 'no',
+      '1, 2 : left' : 'no', 
+      '1, 3 : left' : 'no',
+      '3, 3 : right' : 'no',
+      '3, 2 : right' : 'no',
+      '3, 1 : right' : 'no',
+      '2, 1 : up' : 'no',
+      '3, 1 : up' : 'no',
+      '1, 1 : up' : 'no',
+      '3, 3 : down' : 'no',
+      '2, 3 : down' : 'no',
+      '1, 3 : down' : 'no',
+    }
+
+    if (nonMovableMap[checkMove] === 'no') {
+      setMessage(`You can't go ${direction}`)
+    } else {
+      move(direction)
+    }
   }
 
-  function move(evt) {
+  function move(direction) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    if (direction === 'left') {
+      setSelectedIndex(selectedIndex - 1)
+    } else if (direction === 'up'){
+      setSelectedIndex(selectedIndex - 3)
+    } else if (direction === 'right'){
+      setSelectedIndex(selectedIndex + 1)
+    } else if (direction === 'down'){
+      setSelectedIndex(selectedIndex + 3)
+    }
+    setStepCount(stepCount + 1)
   }
 
   function onChange(e) {
@@ -67,8 +107,8 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">{getXYMessage()}</h3>
+        <h3 id="steps">You moved {stepCount} times</h3>
       </div>
       <div id="grid">
         {
@@ -82,14 +122,14 @@ export default function AppFunctional(props) {
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={(e) => {getNextIndex(e.target.id)}}>LEFT</button>
+        <button id="up" onClick={(e) => {getNextIndex(e.target.id)}}>UP</button>
+        <button id="right" onClick={(e) => {getNextIndex(e.target.id)}}>RIGHT</button>
+        <button id="down" onClick={(e) => {getNextIndex(e.target.id)}}>DOWN</button>
+        <button id="reset" onClick={() => {reset('reset')}}>reset</button>
       </div>
       <form onSubmit={onSubmit}>
         <input id="email" type="email" name="email" value={email.email} onChange={onChange} placeholder="type email"></input>
